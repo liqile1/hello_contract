699bc7f8d220a91ed0a804ec888eee1c36a27ff7
liqile1 <walawalasouk@163.com>
2019-02-18 14:01:17 +0800

    test

--- election/test/election.js
+++ election/test/election.js
@@ -2,6 +2,7 @@ var Election = artifacts.require("./Election.sol")
 
 contract("Election", function(accounts) {
     var electionInstance;
+    // var candidateId
     it("initializes with two cndidates", function() {
         return Election.deployed().then(function(instance) {
             return instance.candidatesCount();
@@ -22,6 +23,22 @@ contract("Election", function(accounts) {
             assert.equal(candidate[0], 2, "contains the correct id");
             assert.equal(candidate[1], "Candidate 2", "contains the correct name");
             assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+    });
+
+    it("allows a voter to cast a vote", function() {
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, {from: accounts[0]});
+        }).then(function(receipt) {
+            return electionInstance.voters(accounts[0]);
+        }).then(function(voted) {
+            assert(voted, "the voter was marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "increments the candidate's vote count");
         });
     });
 });
