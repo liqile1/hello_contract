07fbccd885afd205717e45106be2b916486721e2
liqile1 <walawalasouk@163.com>
2019-02-18 11:31:07 +0800

    test

--- election/test/election.js
+++ election/test/election.js
@@ -1,11 +1,25 @@
 var Election = artifacts.require("./Election.sol")
 
 contract("Election", function(accounts) {
+    var electionInstance;
     it("initializes with two cndidates", function() {
         return Election.deployed().then(function(instance) {
             return instance.candidatesCount();
         }).then(function(count) {
             assert.equal(count, 2);
         })
+    });
+    return Election.deployed().then(function(instance) {
+        electionInstance = instance;
+        return electionInstance.candidates(1);
+    }).then(function(candidate){
+        assert.equal(candidate[0], 1, "contains the correct id");
+        assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+        assert.equal(candidate[2], 0, "contains the correct votes count");
+        return electionInstance.candidates(2);
+    }).then(function(candidate){
+        assert.equal(candidate[0], 2, "contains the correct id");
+        assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+        assert.equal(candidate[2], 0, "contains the correct votes count");
     });
 });
